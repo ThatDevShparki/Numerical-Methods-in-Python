dc3c43b835d1133b03607dfd8311324c3b3d1a34
Matt "Shparki" Murray <murrmat@gmail.com>
2019-02-08 08:10:20 -0600

    Initial Commit

---
+++ nmipy/utils/vector.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Vector():
+
+
+# Factory methods
+@classmethod
+def f_from_nparray(cls, nparray):
+	pass
+
+def f_from_val(cls, n, x):
+	pass
+
+
+# Class Methods
+@classmethod
+def add(cls, u, v):
+	pass
+
+@classmethod
+def sub(cls, u, v):
+	pass
+
+@classmethod
+def dot(cls, u, v):
+	pass
+
+@classmethod
+def norm_l2(cls, u, v):
+	pass
+
+@classmethod
+def norm_li(cls, u, v):
+	pass
+
+
+
+
+# Object Methods
+
+def __init__(self, v_list):
+	pass
